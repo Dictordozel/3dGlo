889dbe288f01e3feb38e683fe15d0b216164a37d
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-02-25 23:44:50 +0200

    Get slider

--- script/script.js
+++ script/script.js
@@ -57,7 +57,7 @@ const toggleMenu = () => {
     const btnMenu = document.querySelector('.menu'),
     menu = document.querySelector('menu');
 
-    btnMenu.addEventListener('click', () =>{
+    btnMenu.addEventListener('click', () => {
         menu.classList.toggle('active-menu');
     });
     menu.addEventListener('click', (event) => {
@@ -82,21 +82,23 @@ const togglePopUp = () => {
 
     let top = 100;
     let popupAnim = () => {
-    top--;
+    top -= 2;
     popupContent.style.top = top + '%';
     if(top > 10) {
-        setTimeout(popupAnim, 0.1);
+        requestAnimationFrame(popupAnim);
         }
     };
+    requestAnimationFrame(popupAnim);
 
     let count = 0.1; 
     let popupOpacity = () => {
         count += 0.01;
         popupContent.style.opacity = count;
         if(count <= 1) {
-            setTimeout(popupOpacity, 10);
+        requestAnimationFrame(popupOpacity);
         } 
     };
+    requestAnimationFrame(popupOpacity);
 
     popupAnim();
     popupOpacity();
@@ -164,6 +166,118 @@ const tabs = () => {
 };
 
 tabs();
+
+// Slider
+
+const slider = () => {
+    const slide = document.querySelectorAll('.portfolio-item'),
+    slider = document.querySelector('.portfolio-content'),
+    portfolioDots = document.querySelector('.portfolio-dots');
+    let dot;
+
+    let currentSlide = 0,
+    interval;
+
+
+    const addDots = () => {
+        for(let i = 0; i < slide.length; i++) {
+            let dots = document.createElement('li');
+            dots.classList.add('dot');
+            portfolioDots.appendChild(dots);
+            }
+            dot = document.querySelectorAll('.dot');
+            dot[0].classList.add(('dot-active'));
+        };
+
+    addDots();
+
+
+    const prevSlide = (elem, index, strClass) => {
+        elem[index].classList.remove(strClass);
+    };
+    const nextSlide = (elem, index, strClass) => {
+        elem[index].classList.add(strClass);
+    };
+
+    const autoPlaySlider = () => {
+
+        prevSlide(slide, currentSlide, 'portfolio-item-active');
+        prevSlide(dot, currentSlide, 'dot-active');
+        currentSlide++;
+        if(currentSlide >= slide.length) {
+            currentSlide = 0; 
+        }
+        nextSlide(slide, currentSlide, 'portfolio-item-active');
+        nextSlide(dot, currentSlide, 'dot-active');
+
+    };
+
+    const startSlide = (time = 3000) => {
+        interval = setInterval(autoPlaySlider, time);
+
+    };
+
+    const stopSlide = () => {
+        clearInterval(interval);
+    };
+
+
+    slider.addEventListener('click', (event) => {
+        event.preventDefault();
+
+        let target = event.target;
+
+        if(!target.matches('.portfolio-btn, .dot')) {
+            return;
+        }
+
+        prevSlide(slide, currentSlide, 'portfolio-item-active');
+        prevSlide(dot, currentSlide, 'dot-active');
+
+        if(target.matches('#arrow-right')) {
+            currentSlide++;
+        } else if(target.matches('#arrow-left')) {
+            currentSlide--;
+        } else if(target.matches('.dot')) {
+            dot.forEach((elem, index) => {
+                if(elem === target) {
+                    currentSlide = index;
+                }
+            });
+        }
+
+        if(currentSlide >= slide.length) {
+            currentSlide = 0;
+        }
+        if(currentSlide < 0) {
+            currentSlide = slide.length - 1;
+        }
+
+        nextSlide(slide, currentSlide, 'portfolio-item-active');
+        nextSlide(dot, currentSlide, 'dot-active');
+    });
+
+    slider.addEventListener('mouseover', (event) => {
+        if(event.target.matches('.portfolio-btn, .dot')) {
+            stopSlide();
+            console.log(event.target);
+            }
+    });
+        
+
+    slider.addEventListener('mouseout', (event) => {
+        if(event.target.matches('.portfolio-btn, .dot')) {
+            startSlide();
+            console.log(event.target);
+        }  
+    });
+
+    
+
+        startSlide(1500);
+    };
+
+slider();
 
 });
 
