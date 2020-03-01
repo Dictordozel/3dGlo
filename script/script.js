4b1f03426c55aa68df035a24396259a64b2a5283
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-03-01 12:51:17 +0200

    Add calc

--- script/script.js
+++ script/script.js
@@ -260,15 +260,13 @@ const slider = () => {
     slider.addEventListener('mouseover', (event) => {
         if(event.target.matches('.portfolio-btn, .dot')) {
             stopSlide();
-            console.log(event.target);
-            }
+        }
     });
         
 
     slider.addEventListener('mouseout', (event) => {
         if(event.target.matches('.portfolio-btn, .dot')) {
             startSlide();
-            console.log(event.target);
         }  
     });
 
@@ -313,5 +311,62 @@ const validCalcValue = () => {
 };
 
 validCalcValue();
+
+// Калькулятор
+
+const calc = (price = 100) => {
+
+    const calcBlock = document.querySelector('.calc-block'),
+        calcType = document.querySelector('.calc-type'),
+        calcSquare = document.querySelector('.calc-square'),
+        calcCount = document.querySelector('.calc-count'),
+        calcDay = document.querySelector('.calc-day'),
+        totalValue = document.getElementById('total');
+
+        const countSum = () => {
+            let total = 0,
+            countValue = 1,
+            dayValue = 1;
+            const typeValue = calcType.options[calcType.selectedIndex].value,
+            squareValue = +calcSquare.value;
+
+            if(calcCount.value > 1) {
+                countValue += (calcCount.value - 1) / 10;
+            }
+
+            if(calcDay.value && calcDay.value < 5) {
+                dayValue *= 2;
+            } else if (calcDay.value && calcDay.value < 10) {
+                dayValue *= 1.5;
+            }
+
+            if(typeValue && squareValue) {
+                total = price * typeValue * squareValue * countValue * dayValue;
+            } 
+            
+            totalValue.textContent = total;
+        };
+
+        calcBlock.addEventListener('change', (event) => {
+        const target = event.target;
+
+        // if(target.matches('.calc-type') || target.matches('.calc-square') || 
+        // target.matches('.calc-count') || target.matches('.calc-day')) {
+        //     console.log(123);
+        // }
+
+        // if(target === calcType || target === calcSquare || 
+        //     target === calcCount || target === calcDay) {
+        //     console.log(456);
+        // }
+
+        if(target.matches('input') || target.matches('select')) {
+            countSum();
+        }
+
+        });   
+};
+
+calc(100);
 
 });
