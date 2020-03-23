c50380b42132302a823414cadf4d5f4ab55dec99
Ivan Chutkan <alexandrbozhkov1977@gmail.com>
2020-03-23 07:27:51 +0200

    Rewrite promise to fetch

--- script/script.js
+++ script/script.js
@@ -405,41 +405,23 @@ window.addEventListener('DOMContentLoaded', () => {
             });   
         };
 
-        const postData = (body) => {
-            return new Promise((resolve, reject) => {
-
-                const request = new XMLHttpRequest();
-                
-            request.addEventListener('readystatechange', () => {
-        
-                if(request.readyState !== 4) {
-                    return;
-                }
-                if(request.status === 200) {
-                    resolve();
-                } else {
-                    reject(request.status);
-                }
-            });
-        
-            request.open('POST', './server.php');
-            request.setRequestHeader('Content-Type', 'application/json');
-            request.send(JSON.stringify(body));
-
+        const postData = (formData) => {
+            return fetch('./server.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: formData
             });
         };
-            
-            
+                 
         const statusMessage = document.createElement('div');
             statusMessage.style.cssText = 'font-size: 2rem;';
             statusMessage.style.color = "#fff";
     
-    
-    
     allForms.forEach((elem) => {
         elem.addEventListener('submit', (event) => {
             event.preventDefault();
-            console.log(elem);
     
             elem.appendChild(statusMessage);
             setTimeout(() => {
@@ -447,15 +429,16 @@ window.addEventListener('DOMContentLoaded', () => {
             }, 3000);
             
             const formData = new FormData(elem);
-            let body = {};
-    
-            formData.forEach((val, key) => {
-                body[key] = val;
-            });
+
+            console.log(formData);
+          
             statusMessage.textContent = loadMessage;
 
-            postData(body)
-            .then(() => {
+            postData(formData)
+            .then((response) => {
+                if(response.status !== 200) { 
+                throw new Error('Status network: ' + response.status);
+                }
                 statusMessage.textContent = successeMessage;
             })
             .catch((error) => {
